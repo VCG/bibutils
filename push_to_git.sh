7461c4ab1fae821b5a48e8e11b4c407ef556816a
Jakob Troidl <jakob.troidl@googlemail.com>
2022-06-27 19:28:49 -0400

    .

--- push_to_git.sh
+++ push_to_git.sh
@@ -1,6 +1,7 @@
 source ~/anaconda3/etc/profile.d/conda.sh
 conda activate bibutils
 python coi.py --first Hanspeter --last Pfister
+git add .
 git commit -m "... updated conflict of interest for Hanspeter Pfister"
 git push
 conda deactivate
