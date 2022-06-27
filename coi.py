2286b9922ae5c38b8d7d108e103369cedaf0b7d2
Jakob Troidl <jakob.troidl@googlemail.com>
2022-06-27 17:31:03 -0400

    automatically download bibtex file

--- coi.py
+++ coi.py
@@ -1,24 +1,30 @@
 import argparse
 from datetime import date
 from bibtexparser.bparser import BibTexParser
+import urllib.request
 
 parser = argparse.ArgumentParser(description='Extract conflict of interest for a given author')
 parser.add_argument('--first', type=str, help='Author first name')
 parser.add_argument('--last', type=str, help='Author last name')
-parser.add_argument('--i', type=str, help='Path to the bibtex file')
 parser.add_argument('--sy', type=int, default=date.today().year - 5, help='Start year to consider conflict of interest')
 parser.add_argument('--ey', type=int, default=date.today().year, help='End year to consider conflict of interest')
 
 args, _ = parser.parse_known_args()
 
 if __name__ == '__main__':
+    file_name = 'papers.bib'
     first_name = args.first
     last_name = args.last
-    bibtex_file = args.i
+    url = 'https://www.zootle.me/bib?groupID=4672801&privateKey=auSCtFH3Dy7Tz32KhwuLNtg1'
 
     print('Extracting conflict of interest for {}'.format(first_name + ' ' + last_name))
 
-    with open(bibtex_file) as bibtex_file:
+    # Download bibtex file
+    print('Downloading bibtex file...')
+    urllib.request.urlretrieve(url, file_name)
+    print('Done.')
+
+    with open(file_name) as bibtex_file:
         bibtex_str = bibtex_file.read()
 
     bp = BibTexParser(interpolate_strings=False)
@@ -40,16 +46,10 @@ if __name__ == '__main__':
     # sort list alphabetically
     conflicting_authors.sort()
 
-    # print list of conflicting authors
-    print('Conflicting authors:')
-    for author in conflicting_authors:
-        print(author)
-
     # write list of conflicting authors to file
     with open('conflicting_authors.txt', 'w') as f:
-        f.write('Conflict of interest for {}'.format(first_name + ' ' + last_name) + ' from {} to {}:\n'.format(args.sy, args.ey))
+        f.write('Conflict of interest for {}'.format(first_name + ' ' + last_name) + ' from {} to {}:\n'.format(args.sy,                                                                                              args.ey))
         for author in conflicting_authors:
             f.write(author + '\n')
 
-
-
+    print('Results written to conflicting_authors.txt')
